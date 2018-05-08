267ec1ef6bdace4e3be0800f5680f7fd56e290a3
nosmanbegovic <nosmanbego1@etf.unsa.ba>
2018-05-08 05:23:56 +0200

    GostHotelaModelView

---
+++ Projekat/LanacHotela/LanacHotela/GostHotelaViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanacHotela
+{
+    class GostHotelaViewModel
+    {
+        public void DodajRezervaciju()
+        {
+
+        }
+
+        public void IzbrisiRezervaciju()
+        {
+
+        }
+
+        public void DodajKomentar()
+        {
+            
+        }
+
+        public void DodajOcjenu()
+        {
+
+        }
+
+        public void RezervisiUslugu()
+        {
+
+        }
+
+        public List<RezervacijaSmjestaja> MojeRezervacije(GostHotela g, LanacHotela l)
+        {
+            List<RezervacijaSmjestaja> lista = new List<RezervacijaSmjestaja>();
+            foreach(Hotel x in l.ListaHotela)
+            {
+                foreach(RezervacijaSmjestaja r in x.ListaRezervacija)
+                {
+                    if (r.Korisnik == g) lista.Add(r);
+                }
+            }
+            return lista;
+        }
+    }
+}
